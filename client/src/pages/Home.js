6ccf76d82c525c6696509b3fd327ac667665509a
saberali1789 <78509035+saberali1789@users.noreply.github.com>
2023-07-08 02:43:41 +0300

    home style

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,27 +1,28 @@
 import { useState, useEffect } from "react"
 import "../home.css"
 
-const Url = 'http://localhost:3001'
+// const http://localhost:3001/ = 'http://localhost:3001'
 
 const Home = () => {
     
-  const [todos, setTodos] = useState([]);
+	const [todos, setTodos] = useState([]);
   const [popupActive, setPopupActive] = useState(false);
   const [newTodo, setNewTodo] = useState("");
 
   useEffect(() => {
+
     GetTodos();
   }, []);
 
   const GetTodos = () => {
-    fetch(URL + "/todos")
+    fetch("http://localhost:3001/todos")
       .then((res) => res.json())
-      .then((data) => setTodos(data))
+      .then((data) => setTodos(data.response))
       .catch((err) => console.error("Error: ", err));
   };
 
   const completeTodo = async (id) => {
-    const data = await fetch(URL + "/todo/complete/" + id).then((res) =>
+    const data = await fetch(`http://localhost:3001/todo/complete/${id}`).then((res) =>
       res.json()
     );
 
@@ -37,7 +38,7 @@ const Home = () => {
   };
 
   const addTodo = async () => {
-    const data = await fetch(URL + "/todo/new", {
+    const data = await fetch("http://localhost:3001/todos", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -54,7 +55,7 @@ const Home = () => {
   };
 
   const deleteTodo = async (id) => {
-    const data = await fetch(URL + "/todo/delete/" + id, {
+    const data = await fetch(`http://localhost:3001/todo/delete/${id}`, {
       method: "DELETE",
     }).then((res) => res.json());
 
@@ -72,39 +73,40 @@ const Home = () => {
             {/* <input type="text" onChange={handleChange} /> */}
 
             <input type="text" className="add-todo-input" onChange={e => setNewTodo(e.target.value)} value={newTodo} />
-            <div className="button" onClick={addTodo}>Create Task</div>
+						<div className="button" onClick={addTodo}>Create Task</div>
 
           </form>
         </div>
 
         
-      <div className="todos">
-        {todos.length > 0 ? todos.map(todo => (
-          <div className={
-            "todo" + (todo.complete ? " is-complete" : "")
-          } key={todo._id} onClick={() => completeTodo(todo._id)}>
-            <div className="checkbox"></div>
+			<div className="todos">
+				{todos.length > 0 ? todos.map(todo => (
+					<div className={
+						"todo" + (todo.complete ? " is-complete" : "")
+					} key={todo._id} onClick={() => completeTodo(todo._id)}>
+						<div className="checkbox"></div>
 
-            <div className="text">{todo.text}</div>
+						<div className="text">{todo.text}</div>
 
-            <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>x</div>
-          </div>
-        )) : (
-          <p>You currently have no tasks</p>
-        )}
-      </div>
+						<div className="delete-todo" onClick={() => deleteTodo(todo._id)}>x</div>
+					</div>
+				)) : (
+					<p>You currently have no tasks</p>
+				)}
+			</div>
 
 
-      
-      
-          
-      
-    </div>
+			
+			
+					
+			
+		</div>
 
     )
 
 
 }
- 
+
+
 
 export default Home
